9750927f7e91c279282467c574bf0c2d937b0311
Chibuisi Enyia <enyia21@gmail.com>
2020-05-01 21:22:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
+require "pry"
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = transfer_amount
+    @status = "pending"
+    @transactions = 0
+  end
+
+  def valid?()
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+    if sender.balance < self.amount
+      self.status = "rejected"
+      sender.status = "rejected"
+
+    end
+    binding.pry
+    if status == "pending" && sender.valid? && receiver.valid?
+      sender.deposit(-self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    elsif sender.valid? && receiver.valid? == false
+      "Transaction rejected. Please check your account balance."
+    else
+      self.status
+    end
+  end
 end
